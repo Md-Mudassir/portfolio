80844a75ef4b86ceb6b58265cfe5b7b7178031dd
Mohammed Mudassir <mdmudassirpro@gmail.com>
2023-06-14 12:02:13 +0530

    minor changes

--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -1,65 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { images } from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
-import { client } from '../../client';
 import './Footer.scss';
 import { BsInstagram, BsLinkedin } from 'react-icons/bs';
 import { FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
-  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [danger, setDanger] = useState(false);
-
-  const [emailError, setEmailError] = useState('');
-  const { name, email, message } = formData;
-
-  const handleChangeInput = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-    setDanger(false);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
-
-    const contact = {
-      _type: 'contact',
-      name: formData.name,
-      email: formData.email,
-      message: formData.message,
-    };
-
-    if (formData.name === '' || formData.message === '' || formData.email === '') {
-      setLoading(false);
-      setDanger(true);
-      return;
-    }
-
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-    if (!formData.email.match(mailformat)) {
-      setLoading(false);
-      setEmailError('Email id is invalid');
-      setDanger(true);
-      return;
-    }
-
-    client
-      .create(contact)
-      .then(() => {
-        setLoading(false);
-        setIsFormSubmitted(true);
-      })
-      .catch((err) => console.log(err));
-  };
-
   return (
     <>
-      <h2 className='head-text'>Take a coffee & chat with me</h2>
+      <h2 className='head-text'>Let's Connect</h2>
 
       <div className='app__footer-cards'>
         <div className='app__footer-card '>
@@ -80,53 +30,6 @@ const Footer = () => {
           <BsInstagram />
         </a>
       </div>
-      {!isFormSubmitted ? (
-        <form className='app__footer-form app__flex'>
-          <div className='app__flex'>
-            <input
-              className='p-text'
-              name='name'
-              type='text'
-              placeholder='Your name'
-              value={name}
-              onChange={handleChangeInput}
-              required
-            />
-          </div>
-          <div className='app__flex'>
-            <input
-              className={`${danger ? 'danger' : 'p-text'}`}
-              name='email'
-              type='email'
-              placeholder='Your email'
-              value={email}
-              onChange={handleChangeInput}
-              required
-            />
-            {emailError && danger && (
-              <div className='app__flex'>
-                <p className='text-danger'>{emailError}</p>
-              </div>
-            )}
-          </div>
-          <div>
-            <textarea
-              name='message'
-              value={message}
-              onChange={handleChangeInput}
-              placeholder='Your Message'
-              className='p-text'
-              required
-            />
-          </div>
-
-          <button type='submit' className='p-text' onClick={handleSubmit}>
-            {!loading ? 'Send Message' : 'Sending...'}
-          </button>
-        </form>
-      ) : (
-        <h3 className='thankyou'>Thank you for getting in touch</h3>
-      )}
     </>
   );
 };
